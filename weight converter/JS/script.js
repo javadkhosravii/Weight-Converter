c1275b71495b707ec033ee76939683ec9931d949
Javad <javadkhosravi777@gmail.com>
2023-10-11 15:22:05 +0100

    first commit

---
+++ weight converter/JS/script.js
@@ -0,0 +1,11 @@
+// hidden the menu
+document.getElementById('output').style.visibility = 'hidden'
+
+document.getElementById('input').addEventListener('click', () => {
+    document.getElementById('output').style.visibility = 'visible'
+    let pounds = input.value; //get the value of the the input
+    // console.log(pounds);
+    document.getElementById('gramsOutput').innerHTML = pounds/0.0022046;
+    document.getElementById('kgOutput').innerHTML = pounds * 0.453592;
+    document.getElementById('ozOutput').innerHTML = pounds*16;
+})
